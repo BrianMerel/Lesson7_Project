75346de342cea9839174eb0c3778c9d1ba30c0db
bm13n <bm13n@my.fsu.edu>
2015-07-17 22:30:29 +0000

    Lesson 7 Project - first commit

---
+++ Lesson7_Assignment_Student/script.js
@@ -0,0 +1,29 @@
+/**
+*Program Name: Recipe Display Application 
+*Author: Brian Merel    
+*Date: 07/17/2015
+*Filename: script.js
+*/
+function display(event){
+
+    $(event.currentTarget).next().fadeIn("slow");
+
+}//end of display()
+
+//$("h3").click(display);
+
+//$("h3").click(display2);
+
+$("h3").hover(display2);
+
+function display2(event) {
+
+   $(event.currentTarget).next().animate( { height: 'toggle'}, "slow");
+
+}
+
+$("h3").hover(function(){
+       $(this).css("background-color", "yellow");
+       }, function(){
+       $(this).css("background-color", "pink");
+});
